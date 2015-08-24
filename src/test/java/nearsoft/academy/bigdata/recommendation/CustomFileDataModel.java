1ad9bd972f943d7542108d0951050f711e40668b
Ariel-isaacm <ariel.isaacm@gmail.com>
2015-08-24 15:57:39 -0700

    Some little changes

--- src/test/java/nearsoft/academy/bigdata/recommendation/CustomFileDataModel.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/CustomFileDataModel.java
@@ -3,6 +3,7 @@ package nearsoft.academy.bigdata.recommendation;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
+import org.apache.mahout.cf.taste.impl.common.FastIDSet;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.common.iterator.FileLineIterator;
 
@@ -10,16 +11,22 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by fernando on 21/08/15.
+ * Created by Ariel Isaac Machado on 21/08/15.
  */
 public class CustomFileDataModel extends FileDataModel {
     private BiMap<String, String> users;
     private BiMap<String, String> products;
+    private File dataFile;
 
 
     public CustomFileDataModel(File DataFile) throws IOException {
-        super(DataFile);
+        super(DataFile, false, 60 * 1000L, " ");
+        this.dataFile = DataFile;
+    }
 
+    @Override
+    protected void processFileWithoutID(FileLineIterator dataOrUpdateFileIterator, FastByIDMap<FastIDSet> data, FastByIDMap<FastByIDMap<Long>> timestamps) {
+        processFile(dataOrUpdateFileIterator, data, timestamps, true);
     }
 
     @Override
@@ -29,6 +36,7 @@ public class CustomFileDataModel extends FileDataModel {
         System.out.println("Reading file info...");
         int count = 0;
         String aux[] = {"", ""};
+
         while (dataOrUpdateFileIterator.hasNext()) {
             String line = dataOrUpdateFileIterator.next();
             if (line.contains("product/productId:")) {
@@ -43,10 +51,11 @@ public class CustomFileDataModel extends FileDataModel {
                     users.put(aux[0], users.size() + 1 + "");
                 }
             } else if (line.contains("review/score:")) {
-                processLine(users.get(aux[0]) + "," + products.get(aux[1]) + "," + line.substring(line.lastIndexOf(' ') + 1), data, timestamps, fromPriorData);
+                processLine(users.get(aux[0]) + " " + products.get(aux[1]) + " " + line.substring(line.lastIndexOf(' ') + 1), data, timestamps, fromPriorData);
                 if (++count % 1000000 == 0) {
 
                     System.out.println("Processed " + count + " lines");
+                    break;
 
                 }
 
@@ -56,7 +65,6 @@ public class CustomFileDataModel extends FileDataModel {
         System.out.println("Read lines: " + count);
 
     }
-
 
 }
 
